f8bffd6acf77d31bd742f27a8737a5c73eb888af
Eris <erisapps@outlook.be>
2021-07-10 01:12:18 +0200

    Revert "Cleaned up Plugin entrypoint"
    This reverts commit 64426398be86e6152dd95390a6144ca31d5e8d2b.

--- MorePlaylists/Plugin.cs
+++ MorePlaylists/Plugin.cs
@@ -1,37 +1,132 @@
 ﻿using IPA;
-using IPA.Logging;
+using IPA.Config;
+using IPA.Config.Stores;
 using MorePlaylists.Installers;
 using MorePlaylists.Utilities;
 using SiraUtil.Zenject;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using UnityEngine;
+using IPALogger = IPA.Logging.Logger;
 
 namespace MorePlaylists
 {
     [Plugin(RuntimeOptions.DynamicInit)]
     public class Plugin
     {
-       internal static Logger Log { get; private set; }
+        // TODO: If using Harmony, uncomment and change YourGitHub to the name of your GitHub account, or use the form "com.company.project.product"
+        //       You must also add a reference to the Harmony assembly in the Libs folder.
+        // public const string HarmonyId = "com.github.YourGitHub.MorePlaylists";
+        // internal static readonly HarmonyLib.Harmony harmony = new HarmonyLib.Harmony(HarmonyId);
 
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        [Init]
         /// <summary>
         /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
         /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
         /// Only use [Init] with one Constructor.
         /// </summary>
-        [Init]
-        public Plugin(Logger logger, Zenjector zenjector)
+        public Plugin(IPALogger logger, Zenjector zenjector)
         {
-            Log = logger;
-            Log.Debug("MorePlaylists initialized.");
+            Instance = this;
+            Plugin.Log = logger;
+            Plugin.Log?.Debug("MorePlaylists initialized.");
             zenjector.OnMenu<MorePlaylistsMenuInstaller>();
             DownloaderUtils.Init();
         }
-        
-        /// <summary>
-        /// Called when the plugin state changes (Plugin getting enabled/disabled or the game starts or exits).
-        /// </summary>
-        [OnEnable, OnDisable]
-        public void OnStateChanged()
+
+        #region BSIPA Config
+        //Uncomment to use BSIPA's config
+        /*
+        [Init]
+        public void InitWithConfig(Config conf)
         {
-            // NOP, SiraUtil/Zenject is poggies
+            Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
+            Plugin.Log?.Debug("Config loaded");
         }
+        */
+        #endregion
+
+
+        #region Disableable
+
+        /// <summary>
+        /// Called when the plugin is enabled (including when the game starts if the plugin is enabled).
+        /// </summary>
+        [OnEnable]
+        public void OnEnable()
+        {
+            //ApplyHarmonyPatches();
+        }
+
+        /// <summary>
+        /// Called when the plugin is disabled and on Beat Saber quit. It is important to clean up any Harmony patches, GameObjects, and Monobehaviours here.
+        /// The game should be left in a state as if the plugin was never started.
+        /// Methods marked [OnDisable] must return void or Task.
+        /// </summary>
+        [OnDisable]
+        public void OnDisable()
+        {
+            //RemoveHarmonyPatches();
+        }
+
+        /*
+        /// <summary>
+        /// Called when the plugin is disabled and on Beat Saber quit.
+        /// Return Task for when the plugin needs to do some long-running, asynchronous work to disable.
+        /// [OnDisable] methods that return Task are called after all [OnDisable] methods that return void.
+        /// </summary>
+        [OnDisable]
+        public async Task OnDisableAsync()
+        {
+            await LongRunningUnloadTask().ConfigureAwait(false);
+        }
+        */
+        #endregion
+
+        // Uncomment the methods in this section if using Harmony
+        #region Harmony
+        /*
+        /// <summary>
+        /// Attempts to apply all the Harmony patches in this assembly.
+        /// </summary>
+        internal static void ApplyHarmonyPatches()
+        {
+            try
+            {
+                Plugin.Log?.Debug("Applying Harmony patches.");
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                Plugin.Log?.Error("Error applying Harmony patches: " + ex.Message);
+                Plugin.Log?.Debug(ex);
+            }
+        }
+
+        /// <summary>
+        /// Attempts to remove all the Harmony patches that used our HarmonyId.
+        /// </summary>
+        internal static void RemoveHarmonyPatches()
+        {
+            try
+            {
+                // Removes all patches with this HarmonyId
+                harmony.UnpatchAll(HarmonyId);
+            }
+            catch (Exception ex)
+            {
+                Plugin.Log?.Error("Error removing Harmony patches: " + ex.Message);
+                Plugin.Log?.Debug(ex);
+            }
+        }
+        */
+        #endregion
     }
 }
