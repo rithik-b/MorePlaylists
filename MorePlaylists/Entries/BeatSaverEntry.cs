573926e64742245921b321bc0dcd12812c52a3de
PixelBoom <rithikbhatia12@gmail.com>
2021-12-22 04:31:05 +0400

    Caching songs

--- MorePlaylists/Entries/BeatSaverEntry.cs
+++ MorePlaylists/Entries/BeatSaverEntry.cs
@@ -51,40 +51,45 @@ namespace MorePlaylists.Entries
             protected set { }
         }
 
+        private List<Song> songs;
+
         public override async Task<List<Song>> GetSongs(IHttpService siraHttpService)
         {
-            List<Song> songs = new List<Song>();
-            int page = 0;
-            bool limitReached = false;
-            while(!limitReached)
+            if (songs == null)
             {
-                try
+                songs = new List<Song>();
+                int page = 0;
+                bool limitReached = false;
+                while (!limitReached)
                 {
-                    IHttpResponse webResponse = await siraHttpService.GetAsync($"https://api.beatsaver.com/playlists/id/{PlaylistID}/{page}", cancellationToken: CancellationToken.None);
-                    if (webResponse.Successful)
+                    try
                     {
-                        BeatSaverDetailEntry detailEntry = JsonConvert.DeserializeObject<BeatSaverDetailEntry>(await webResponse.ReadAsStringAsync());
-                        if (detailEntry.maps.Count != 0)
+                        IHttpResponse webResponse = await siraHttpService.GetAsync($"https://api.beatsaver.com/playlists/id/{PlaylistID}/{page}", cancellationToken: CancellationToken.None);
+                        if (webResponse.Successful)
                         {
-                            foreach (BeatSaverMapDetailWithOrder map in detailEntry.maps)
+                            BeatSaverDetailEntry detailEntry = JsonConvert.DeserializeObject<BeatSaverDetailEntry>(await webResponse.ReadAsStringAsync());
+                            if (detailEntry.maps.Count != 0)
                             {
-                                songs.Add(new Song(map.map.Metadata.SongName, $"{map.map.Metadata.SongAuthorName} [{map.map.Metadata.LevelAuthorName}]", map.map.LatestVersion.CoverURL));
+                                foreach (BeatSaverMapDetailWithOrder map in detailEntry.maps)
+                                {
+                                    songs.Add(new Song(map.map.Metadata.SongName, $"{map.map.Metadata.SongAuthorName} [{map.map.Metadata.LevelAuthorName}]", map.map.LatestVersion.CoverURL));
+                                }
                             }
+                            else
+                            {
+                                limitReached = true;
+                            }
+                            page++;
                         }
                         else
                         {
                             limitReached = true;
                         }
-                        page++;
                     }
-                    else
+                    catch (Exception)
                     {
                         limitReached = true;
                     }
-                }
-                catch (Exception)
-                {
-                    limitReached = true;
                 }
             }
             return songs;
