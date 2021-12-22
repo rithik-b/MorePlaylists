0c0041d2e6940449de7186386ae194b65e8cad4d
PixelBoom <rithikbhatia12@gmail.com>
2021-12-22 06:14:30 +0400

    Added AccSaber support

---
+++ MorePlaylists/Entries/AccSaberEntry.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using SiraUtil.Web;
+
+namespace MorePlaylists.Entries
+{
+    internal class AccSaberEntry : SongDetailsEntry
+    {
+        public override string Title { get; protected set; }
+        public override string Author { get; protected set; }
+        public override string Description { get; protected set; }
+        public override string PlaylistURL { get; protected set; }
+        public override string SpriteString { get; protected set; }
+        public override SpriteType SpriteType => SpriteType.Playlist;
+
+        public static async Task<AccSaberEntry> GetAccSaberPlaylist(string playlistURL, IHttpService siraHttpService)
+        {
+            AccSaberEntry accSaberEntry = new AccSaberEntry();
+            accSaberEntry.PlaylistURL = playlistURL;
+            await accSaberEntry.DownloadPlaylist(siraHttpService);
+            if (accSaberEntry.RemotePlaylist != null)
+            {
+                accSaberEntry.Title = accSaberEntry.RemotePlaylist.Title;
+                accSaberEntry.Author = accSaberEntry.RemotePlaylist.Author;
+                accSaberEntry.Description = accSaberEntry.RemotePlaylist.Description;
+                return accSaberEntry;
+            }
+            return null;
+        }
+    }
+}
